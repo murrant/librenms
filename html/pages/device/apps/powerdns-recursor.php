33cf72fd09b8355f1e7bac69cd9749a51c3caa41
Tony Murray <murraytony@gmail.com>
2016-07-01 16:34:17 -0500

    Add graph to apps tab.

--- html/pages/device/apps/powerdns-recursor.php
+++ html/pages/device/apps/powerdns-recursor.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * powerdns-recursor_answers.inc.php
+ * powerdns-recursor.php
  *
- * Graph of answers
+ * -Description-
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -23,36 +23,25 @@
  * @author     Tony Murray <murraytony@gmail.com>
  */
 
-include 'powerdns-recursor.inc.php';
-
-$colours = 'blues';
-
-$rrd_list = array(
-    array(
-        'ds' => 'answers0-1',
-        'filename' => $rrd_filename,
-        'descr' => '0-1ms',
-    ),
-    array(
-        'ds' => 'answers1-10',
-        'filename' => $rrd_filename,
-        'descr' => '1-10ms',
-    ),
-    array(
-        'ds' => 'answers10-100',
-        'filename' => $rrd_filename,
-        'descr' => '10-100ms',
-    ),
-    array(
-        'ds' => 'answers100-1000',
-        'filename' => $rrd_filename,
-        'descr' => '100-1000ms',
-    ),
-    array(
-        'ds' => 'answers-slow',
-        'filename' => $rrd_filename,
-        'descr' => '>1s',
-    ),
+$graphs = array(
+    'powerdns-recursor_answers' => 'Answers',
 );
 
-require 'includes/graphs/generic_multi.inc.php';
+foreach ($graphs as $key => $text) {
+    $graph_array['height'] = '100';
+    $graph_array['width']  = '215';
+    $graph_array['to']     = $config['time']['now'];
+    $graph_array['id']     = $app['app_id'];
+    $graph_array['type']   = 'application_'.$key;
+
+    echo '<div class="panel panel-default">
+    <div class="panel-heading">
+        <h3 class="panel-title">'.$text.'</h3>
+    </div>
+    <div class="panel-body">
+    <div class="row">';
+    include 'includes/print-graphrow.inc.php';
+    echo '</div>';
+    echo '</div>';
+    echo '</div>';
+}
