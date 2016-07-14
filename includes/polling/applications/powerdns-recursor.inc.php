428efac5334f0a79aaf8f7d4a98226af652f57c5
Tony Murray <murraytony@gmail.com>
2016-07-14 15:22:54 -0500

    Convert to data_update()

--- includes/polling/applications/powerdns-recursor.inc.php
+++ includes/polling/applications/powerdns-recursor.inc.php
@@ -29,106 +29,91 @@ echo ' powerdns-recrusor';
 global $config;
 $data = '';
 
-if ($agent_data['app']['powerdns-recursor']) {
-    $data = $agent_data['app']['powerdns-recursor'];
-} elseif(isset($config['apps']['powerdns-recursor']['api-key'])) {
+$name = 'powerdns-recursor';
+$app_id = $app['app_id'];
+if ($agent_data['app'][$name]) {
+    $data = $agent_data['app'][$name];
+} elseif (isset($config['apps'][$name]['api-key'])) {
     d_echo("\nNo Agent Data. Attempting to connect directly to the powerdns-recursor server " . $device['hostname'] . ":8082\n");
-    $context = stream_context_create(array('http' => array('header' => 'X-API-Key: '.$config['apps']['powerdns-recursor']['api-key'])));
+    $context = stream_context_create(array('http' => array('header' => 'X-API-Key: ' . $config['apps'][$name]['api-key'])));
     $data = file_get_contents('http://' . $device['hostname'] . ':8082/servers/localhost/statistics', false, $context);
 }
 
-
 $stats = json_decode($data);
 
-
 if (!empty($stats)) {
-
-$rrd_keys = array(
-    'all-outqueries' => 'COUNTER',
-    'answers-slow' => 'COUNTER',
-    'answers0-1' => 'COUNTER',
-    'answers1-10' => 'COUNTER',
-    'answers10-100' => 'COUNTER',
-    'answers100-1000' => 'COUNTER',
-    'cache-entries' => 'GAUGE',
-    'cache-hits' => 'COUNTER',
-    'cache-misses' => 'COUNTER',
-    'case-mismatches' => 'COUNTER',
-    'chain-resends' => 'COUNTER',
-    'client-parse-errors' => 'COUNTER',
-    'concurrent-queries' => 'GAUGE',
-    'dlg-only-drops' => 'COUNTER',
-    'dont-outqueries' => 'COUNTER',
-    'edns-ping-matches' => 'COUNTER',
-    'edns-ping-mismatches' => 'COUNTER',
-    'failed-host-entries' => 'GAUGE',
-    'ipv6-outqueries' => 'COUNTER',
-    'ipv6-questions' => 'COUNTER',
-    'malloc-bytes' => 'GAUGE',
-    'max-mthread-stack' => 'GAUGE',
-    'negcache-entries' => 'GAUGE',
-    'no-packet-error' => 'COUNTER',
-    'noedns-outqueries' => 'COUNTER',
-    'noerror-answers' => 'COUNTER',
-    'noping-outqueries' => 'COUNTER',
-    'nsset-invalidations' => 'COUNTER',
-    'nsspeeds-entries' => 'GAUGE',
-    'nxdomain-answers' => 'COUNTER',
-    'outgoing-timeouts' => 'COUNTER',
-    'over-capacity-drops' => 'COUNTER',
-    'packetcache-entries' => 'GAUGE',
-    'packetcache-hits' => 'COUNTER',
-    'packetcache-misses' => 'COUNTER',
-    'policy-drops' => 'COUNTER',
-    'qa-latency' => 'GAUGE',
-    'questions' => 'COUNTER',
-    'resource-limits' => 'COUNTER',
-    'security-status' => 'GAUGE',
-    'server-parse-errors' => 'COUNTER',
-    'servfail-answers' => 'COUNTER',
-    'spoof-prevents' => 'COUNTER',
-    'sys-msec' => 'COUNTER',
-    'tcp-client-overflow' => 'COUNTER',
-    'tcp-clients' => 'GAUGE',
-    'tcp-outqueries' => 'COUNTER',
-    'tcp-questions' => 'COUNTER',
-    'throttle-entries' => 'GAUGE',
-    'throttled-out' => 'COUNTER',
-    'throttled-outqueries' => 'COUNTER',
-    'too-old-drops' => 'COUNTER',
-    'unauthorized-tcp' => 'COUNTER',
-    'unauthorized-udp' => 'COUNTER',
-    'unexpected-packets' => 'COUNTER',
-    'unreachables' => 'COUNTER',
-    'uptime' => 'COUNTER',
-    'user-msec' => 'COUNTER',
-);
-
-// only the stats we store in rrd
-$fields = array_intersect_key($stats, $rrd_keys);
-
-$rrd_filename = $config['rrd_dir'].'/'.$device['hostname'].'/app-powerdns-recursor-'.$app['app_id'].'.rrd';
-
-if (!is_file($rrd_filename)) {
-    $rrd_defs = "--step 300 ";
-    foreach ($rrd_keys as $statname => $type) {
-        $rrd_defs .= 'DS:' . substr($statname, 0, 19) . ':' . $type . ":600:0:U ";
-    }
-echo $rrd_defs;
-    rrdtool_create(
-        $rrd_filename,
-        $rrd_defs.$config['rrd_rra']
+    $rrd_keys = array(
+        'all-outqueries' => 'COUNTER',
+        'answers-slow' => 'COUNTER',
+        'answers0-1' => 'COUNTER',
+        'answers1-10' => 'COUNTER',
+        'answers10-100' => 'COUNTER',
+        'answers100-1000' => 'COUNTER',
+        'cache-entries' => 'GAUGE',
+        'cache-hits' => 'COUNTER',
+        'cache-misses' => 'COUNTER',
+        'case-mismatches' => 'COUNTER',
+        'chain-resends' => 'COUNTER',
+        'client-parse-errors' => 'COUNTER',
+        'concurrent-queries' => 'GAUGE',
+        'dlg-only-drops' => 'COUNTER',
+        'dont-outqueries' => 'COUNTER',
+        'edns-ping-matches' => 'COUNTER',
+        'edns-ping-mismatches' => 'COUNTER',
+        'failed-host-entries' => 'GAUGE',
+        'ipv6-outqueries' => 'COUNTER',
+        'ipv6-questions' => 'COUNTER',
+        'malloc-bytes' => 'GAUGE',
+        'max-mthread-stack' => 'GAUGE',
+        'negcache-entries' => 'GAUGE',
+        'no-packet-error' => 'COUNTER',
+        'noedns-outqueries' => 'COUNTER',
+        'noerror-answers' => 'COUNTER',
+        'noping-outqueries' => 'COUNTER',
+        'nsset-invalidations' => 'COUNTER',
+        'nsspeeds-entries' => 'GAUGE',
+        'nxdomain-answers' => 'COUNTER',
+        'outgoing-timeouts' => 'COUNTER',
+        'over-capacity-drops' => 'COUNTER',
+        'packetcache-entries' => 'GAUGE',
+        'packetcache-hits' => 'COUNTER',
+        'packetcache-misses' => 'COUNTER',
+        'policy-drops' => 'COUNTER',
+        'qa-latency' => 'GAUGE',
+        'questions' => 'COUNTER',
+        'resource-limits' => 'COUNTER',
+        'security-status' => 'GAUGE',
+        'server-parse-errors' => 'COUNTER',
+        'servfail-answers' => 'COUNTER',
+        'spoof-prevents' => 'COUNTER',
+        'sys-msec' => 'COUNTER',
+        'tcp-client-overflow' => 'COUNTER',
+        'tcp-clients' => 'GAUGE',
+        'tcp-outqueries' => 'COUNTER',
+        'tcp-questions' => 'COUNTER',
+        'throttle-entries' => 'GAUGE',
+        'throttled-out' => 'COUNTER',
+        'throttled-outqueries' => 'COUNTER',
+        'too-old-drops' => 'COUNTER',
+        'unauthorized-tcp' => 'COUNTER',
+        'unauthorized-udp' => 'COUNTER',
+        'unexpected-packets' => 'COUNTER',
+        'unreachables' => 'COUNTER',
+        'uptime' => 'COUNTER',
+        'user-msec' => 'COUNTER',
     );
+
+    // only the stats we store in rrd
+    $fields = array_intersect_key($stats, $rrd_keys);
+
+    $rrd_name = array('app', 'powerdns', 'recursor', $app_id);
+    $rrd_def = array();
+    foreach ($rrd_keys as $statname => $type) {
+        $rrd_def[] = 'DS:' . substr($statname, 0, 19) . ':' . $type . ":600:0:U ";
+    }
+
+    $tags = compact('name', 'app_id', 'rrd_name', 'rrd_def');
+    data_update($device, 'app', $tags, $fields);
 }
 
-rrdtool_update($rrd_filename, $fields);
-
-$tags = array('name' => 'powerdns-recursor', 'app_id' => $app['app_id']);
-influx_update($device,'app',$tags,$fields);
-
-}
-
-unset($data);
-unset($rrd_filename);
-unset($stats);
-unset($tags);
+unset($data, $stats, $rrd_def, $rrd_name, $rrd_keys, $tags, $fields);
