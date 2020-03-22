2bd70cd79d0e635911d0590c960e033ab3176598
Tony Murray <murraytony@gmail.com>
2020-03-22 10:52:45 -0500

    Reorganize options

--- resources/views/device/create.blade.php
+++ resources/views/device/create.blade.php
@@ -13,13 +13,7 @@
                                 <div class="panel-title">@lang('Add Device')
                                     <div class="pull-right checkbox-inline">
                                         <label>
-                                            <toggle-button
-                                                    name="@lang('Advanced')"
-                                                    :value="advanced"
-                                                    :sync="true"
-                                                    @change="toggleAdvanced"
-                                            ></toggle-button>
-                                            @lang('Advanced')
+                                            <toggle-button name="@lang('Advanced')" :value="advanced" :sync="true" @change="toggleAdvanced"></toggle-button> @lang('Advanced')
                                         </label>
                                     </div>
                                 </div>
@@ -32,7 +26,7 @@
                                         <div class="col-sm-9">
                                             <div class="input-group">
                                                 <input type="text" class="form-control input-sm" id="hostname" placeholder="@lang('Hostname')" v-model="hostname">
-                                                <span class="input-group-addon" id="basic-addon2"><i class="fa fa-fw fa-lg fa-question-circle has-tooltip"></i></span>
+                                                <span class="input-group-addon"><i class="fa fa-fw fa-lg fa-question-circle has-tooltip"></i></span>
                                             </div>
                                         </div>
                                     </div>
@@ -40,7 +34,10 @@
                                     <div class="form-group" v-show="advanced" @if(!$data['advanced']) style="display:none" @endif>
                                         <label for="override_ip" class="col-sm-3 control-label">@lang('Override IP')</label>
                                         <div class="col-sm-9">
-                                            <input type="text" class="form-control input-sm" id="override_ip" placeholder="IP" v-model="override_ip">
+                                            <div class="input-group">
+                                                <input type="text" class="form-control input-sm" id="override_ip" placeholder="IP" v-model="override_ip">
+                                                <span class="input-group-addon"><i class="fa fa-fw fa-lg fa-question-circle has-tooltip"></i></span>
+                                            </div>
                                         </div>
                                     </div>
 
@@ -61,8 +58,10 @@
                                     <div class="form-group" v-show="type === 'snmpv1' || type === 'snmpv2'">
                                         <label for="community" class="col-sm-3 control-label">Community</label>
                                         <div class="col-sm-6">
-                                            <input type="text" class="form-control input-sm" id="community" placeholder="@lang('Leave blank to use default')">
-                                            <i class="fa fa-fw fa-lg fa-question-circle has-tooltip"></i>
+                                            <div class="input-group">
+                                                <input type="text" class="form-control input-sm" id="community" placeholder="@lang('Leave blank to use default')">
+                                                <span class="input-group-addon"><i class="fa fa-fw fa-lg fa-question-circle has-tooltip"></i></span>
+                                            </div>
                                         </div>
                                         <div class="col-sm-3">
                                             <a href="{{ route('settings') . '/poller/snmp' }}">
@@ -71,73 +70,15 @@
                                         </div>
                                     </div>
 
-                                    <div class="form-group" v-show="advanced && type !== 'ping'" @if(!$data['advanced']) style="display:none" @endif>
-                                        <label class="col-sm-3 control-label">@lang('Transport')</label>
-
-                                        <div class="col-sm-3">
-                                            <label class="sr-only" for="port">@lang('Port')</label>
-                                            <input type="text" class="form-control input-sm" id="port" placeholder="@lang('Port')" v-model="port">
-                                        </div>
-                                        <div class="col-sm-3">
-                                            <label for="proto" class="sr-only">@lang('Protocol')</label>
-                                            <button-group class="btn-group btn-group-sm" v-model="proto">
-                                                <button type="button" class="btn btn-sm btn-default active" value="udp">UDP</button>
-                                                <button type="button" class="btn btn-sm btn-default" value="tcp">TCP</button>
-                                            </button-group>
-                                        </div>
-                                        <div class="col-sm-3">
-                                            <label for="ipt" class="sr-only">@lang('IP')</label>
-                                            <button-group class="btn-group btn-group-sm" v-model="transport">
-                                                <button type="button" class="btn btn-sm btn-default active" value="auto">@lang('Auto')</button>
-                                                <button type="button" class="btn btn-sm btn-default" value="4">IPv4</button>
-                                                <button type="button" class="btn btn-sm btn-default" value="6">IPv6</button>
-                                            </button-group>
-                                        </div>
-                                    </div>
-
-                                    <!-- Ping Options -->
-
-                                    <div class="form-group" v-show="advanced && type === 'ping'" style="display:none">
-                                        <label for="sysName" class="col-sm-3 control-label">@lang('sysName')</label>
-                                        <div class="col-sm-9">
-                                            <input type="text" class="form-control input-sm" id="sysName" placeholder="@lang('sysName')" v-model="sysname">
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group" v-show="advanced && type === 'ping'" style="display:none">
-                                        <label for="os" class="col-sm-3 control-label">@lang('OS')</label>
-                                        <div class="col-sm-9">
-                                            <input type="text" class="form-control input-sm" id="os" placeholder="@lang('OS')" v-model="os">
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group" v-show="advanced && type === 'ping'" style="display:none">
-                                        <label for="hardware" class="col-sm-3 control-label">@lang('Hardware')</label>
-                                        <div class="col-sm-9">
-                                            <input type="text" class="form-control input-sm" id="hardware" placeholder="@lang('Hardware')" v-model="hardware">
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group" v-show="advanced" @if(!$data['advanced']) style="display:none" @endif>
-                                        <label for="port_association" class="col-sm-3 control-label">Port Association</label>
-                                        <div class="col-sm-3">
-                                            <select name="port_association" id="port_association" class="form-control input-sm" v-model="port_association">
-                                                @foreach(\App\Models\Port::associationModes() as $mode)
-                                                    <option value="{{ $mode }}">{{ $mode }}</option>
-                                                @endforeach
-                                            </select>
-                                        </div>
-                                    </div>
-
                                     <!-- SNMP v3 Options -->
 
                                     <div class="form-group" v-show="type === 'snmpv3'" style="display:none">
-                                        <label for="auth_level" class="col-sm-3 control-label">Auth Level</label>
+                                        <label for="auth_level" class="col-sm-3 control-label">@lang('Authentication')</label>
                                         <div class="col-sm-9">
                                             <button-group class="btn-group btn-group-sm" v-model="auth_level">
-                                                <button type="button" class="btn btn-sm btn-default active" value="noAuthNoPriv">@lang('noAuthNoPriv')</button>
-                                                <button type="button" class="btn btn-sm btn-default" value="authNoPriv">@lang('authNoPriv')</button>
-                                                <button type="button" class="btn btn-sm btn-default" value="authPriv">@lang('authPriv')</button>
+                                                <button type="button" class="btn btn-sm btn-default active" value="noAuthNoPriv">@lang('None')</button>
+                                                <button type="button" class="btn btn-sm btn-default" value="authNoPriv">@lang('Password')</button>
+                                                <button type="button" class="btn btn-sm btn-default" value="authPriv">@lang('Encrypted')</button>
                                             </button-group>
                                         </div>
                                     </div>
@@ -150,7 +91,7 @@
                                     <div class="form-group" v-show="type === 'snmpv3' && auth_level !== 'noAuthNoPriv'" style="display:none">
                                         <label for="auth_pass" class="col-sm-3 control-label">@lang('Auth Password')</label>
                                         <div class="col-sm-6">
-                                            <input type="text"placeholder="@lang('Password')" id="auth_pass" class="form-control input-sm" autocomplete="off" v-model="auth_pass">
+                                            <input type="text" placeholder="@lang('Password')" id="auth_pass" class="form-control input-sm" autocomplete="off" v-model="auth_pass">
                                         </div>
                                         <div class="col-sm-3" v-show="advanced">
                                             <button-group class="btn-group btn-group-sm" v-model="auth_algo" title="@lang('Auth Algorithm')">
@@ -162,13 +103,89 @@
                                     <div class="form-group" v-show="type === 'snmpv3' && auth_level === 'authPriv'" style="display:none">
                                         <label for="crypto_pass" class="col-sm-3 control-label">@lang('Crypto Password')</label>
                                         <div class="col-sm-6">
-                                            <input type="text" placeholder="@lang('Crypto Password')" id="crypto_pass" class="form-control input-sm" autocomplete="off" v-model="crypto_pass">
+                                            <input type="text" placeholder="@lang('Crypto Password')" id="crypto_pass" class="form-control input-sm" autocomplete="off"
+                                                   v-model="crypto_pass">
                                         </div>
                                         <div class="col-sm-3" v-show="advanced">
                                             <button-group class="btn-group btn-group-sm" v-model="crypto_algo" title="@lang('Crypto Algorithm')">
                                                 <button type="button" class="btn btn-sm btn-default active" value="AES">AES</button>
                                                 <button type="button" class="btn btn-sm btn-default" value="DES">DES</button>
                                             </button-group>
+                                        </div>
+                                    </div>
+
+                                    <!-- SNMP Common Options -->
+
+                                    <div class="form-group" v-show="advanced && type !== 'ping'" @if(!$data['advanced']) style="display:none" @endif>
+                                        <label class="col-sm-3 control-label">@lang('Transport')</label>
+
+                                        <div class="col-sm-3">
+                                            <label class="sr-only" for="port">@lang('Port')</label>
+                                            <div class="input-group">
+                                                <input type="text" class="form-control input-sm" id="port" placeholder="@lang('Port')" v-model="port">
+                                                <span class="input-group-addon"><i class="fa fa-fw fa-lg fa-question-circle has-tooltip"></i></span>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-3">
+                                            <label for="ipt" class="sr-only">@lang('IP')</label>
+                                            <button-group class="btn-group btn-group-sm" v-model="transport">
+                                                <button type="button" class="btn btn-sm btn-default active" value="auto">@lang('Auto')</button>
+                                                <button type="button" class="btn btn-sm btn-default" value="4">IPv4</button>
+                                                <button type="button" class="btn btn-sm btn-default" value="6">IPv6</button>
+                                            </button-group>
+                                        </div>
+                                        <div class="col-sm-3">
+                                            <label for="proto" class="sr-only">@lang('Protocol')</label>
+                                            <button-group class="btn-group btn-group-sm" v-model="proto">
+                                                <button type="button" class="btn btn-sm btn-default active" value="udp">UDP</button>
+                                                <button type="button" class="btn btn-sm btn-default" value="tcp">TCP</button>
+                                            </button-group>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group" v-show="advanced && type !== 'ping'" @if(!$data['advanced']) style="display:none" @endif>
+                                        <label for="port_association" class="col-sm-3 control-label">Port Association</label>
+                                        <div class="col-sm-3">
+                                            <div class="input-group">
+                                                <select name="port_association" id="port_association" class="form-control input-sm" v-model="port_association">
+                                                    @foreach(\App\Models\Port::associationModes() as $mode)
+                                                        <option value="{{ $mode }}">{{ $mode }}</option>
+                                                    @endforeach
+                                                </select>
+                                                <span class="input-group-addon"><i class="fa fa-fw fa-lg fa-question-circle has-tooltip"></i></span>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <!-- Ping Options -->
+
+                                    <div class="form-group" v-show="advanced && type === 'ping'" style="display:none">
+                                        <label for="sysName" class="col-sm-3 control-label">@lang('sysName')</label>
+                                        <div class="col-sm-9">
+                                            <div class="input-group">
+                                                <input type="text" class="form-control input-sm" id="sysName" placeholder="@lang('sysName')" v-model="sysname">
+                                                <span class="input-group-addon"><i class="fa fa-fw fa-lg fa-question-circle has-tooltip"></i></span>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group" v-show="advanced && type === 'ping'" style="display:none">
+                                        <label for="os" class="col-sm-3 control-label">@lang('OS')</label>
+                                        <div class="col-sm-9">
+                                            <div class="input-group">
+                                                <input type="text" class="form-control input-sm" id="os" placeholder="@lang('OS')" v-model="os">
+                                                <span class="input-group-addon"><i class="fa fa-fw fa-lg fa-question-circle has-tooltip"></i></span>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group" v-show="advanced && type === 'ping'" style="display:none">
+                                        <label for="hardware" class="col-sm-3 control-label">@lang('Hardware')</label>
+                                        <div class="col-sm-9">
+                                            <div class="input-group">
+                                                <input type="text" class="form-control input-sm" id="hardware" placeholder="@lang('Hardware')" v-model="hardware">
+                                                <span class="input-group-addon"><i class="fa fa-fw fa-lg fa-question-circle has-tooltip"></i></span>
+                                            </div>
                                         </div>
                                     </div>
 
