d9d7698db26162ddfb33b5f948cca90b6a12ff6f
Tony Murray <murraytony@gmail.com>
2021-01-27 00:11:07 -0600

    device overview

--- app/Http/Livewire/DeviceOverviewWidget.php
+++ app/Http/Livewire/DeviceOverviewWidget.php
@@ -3,6 +3,11 @@
 namespace App\Http\Livewire;
 
 use App\Facades\DeviceCache;
+use LibreNMS\Config;
+use LibreNMS\Exceptions\InvalidIpException;
+use LibreNMS\Util\IP;
+use LibreNMS\Util\Rewrite;
+use LibreNMS\Util\Time;
 use Livewire\Component;
 
 class DeviceOverviewWidget extends Component
@@ -11,11 +16,42 @@ class DeviceOverviewWidget extends Component
      * @var \App\Models\Device
      */
     public $device;
+    public $ip;
+    public $ip_type = 'ip';
+    public $hardware;
+    public $os;
+    public $contact;
+    public $date_added;
 
     public function __construct($id = null)
     {
         parent::__construct($id);
         $this->device = DeviceCache::getPrimary();
+
+        if (! empty($this->device->overwrite_ip)) {
+            $this->ip = $this->device->overwrite_ip;
+            $this->ip_type = 'assigned';
+        } elseif (! empty($this->device->ip)) {
+            $this->ip = $this->device->ip;
+            $this->ip_type = 'resolved';
+        } elseif (Config::get('force_ip_to_sysname') === true) {
+            try {
+                $this->ip = IP::parse($this->device->hostname);
+            } catch (InvalidIpException $e) {
+                // don't add an ip line
+            }
+        }
+
+        $this->hardware = Rewrite::ciscoHardware($this->device);
+
+        $this->os = Config::getOsSetting($this->device->os, 'text') . ' ' . $this->device->version;
+        if ($this->device->features) {
+            $this->os .= " ({$this->device->features})";
+        }
+
+        $this->contact = $this->device->getAttrib('override_sysContact_bool') ? $this->device->getAttrib('override_sysContact_string') : $this->device->sysContact;
+
+        $this->date_added = $this->device->inserted ? (Time::formatInterval(time() - strtotime($this->device->inserted)) . ' ' . __('ago')) : null;
     }
 
     public function render()
--- resources/views/livewire/device-overview-widget.blade.php
+++ resources/views/livewire/device-overview-widget.blade.php
@@ -1,5 +1,5 @@
 <div class="grid-stack-item">
-<div class="panel panel-default shadow">
+<div class="panel panel-default shadow device-overview">
     <div class="panel-heading dash-widget-header">
         <span class="fade-edit pull-right">
             <i class="fa fa-pencil-square-o" data-widget-id="1" aria-label="Settings" data-toggle="tooltip" data-placement="top" title="Settings"></i>
@@ -11,9 +11,57 @@
     </div>
     <div class="grid-stack-item-content panel-body dash-widget-body">
         <div class="row">
-            <div class="col-sm-4">@lang('System Name')</div>
+            <div class="col-sm-4">@lang('device.attributes.sysName')</div>
             <div class="col-sm-8">{{ $device->sysName }}</div>
         </div>
+        @if($ip)
+        <div class='row'>
+            <div class='col-sm-4'>@lang("device.ip.$ip_type")</div>
+            <div class='col-sm-8'>{{ $ip }}</div>
+        </div>
+        @endif
+        @if($device->purpose)
+            <div class="row">
+                <div class="col-sm-4">@lang('device.attributes.purpose')</div>
+                <div class="col-sm-8">{{ $device->purpose }}</div>
+            </div>
+        @endif
+        @if($hardware)
+            <div class="row">
+                <div class="col-sm-4">@lang('device.attributes.hardware')</div>
+                <div class="col-sm-8">{{ $hardware }}</div>
+            </div>
+        @endif
+        @if($os)
+            <div class="row">
+                <div class="col-sm-4">@lang('device.attributes.os')</div>
+                <div class="col-sm-8">{{ $os }}</div>
+            </div>
+        @endif
+        @if($device->serial)
+            <div class="row">
+                <div class="col-sm-4">@lang('device.attributes.serial')</div>
+                <div class="col-sm-8">{{ $device->serial }}</div>
+            </div>
+        @endif
+        @if($device->sysObjectID)
+            <div class="row">
+                <div class="col-sm-4">@lang('device.attributes.sysObjectID')</div>
+                <div class="col-sm-8">{{ $device->sysObjectID }}</div>
+            </div>
+        @endif
+        @if($device->contact)
+            <div class="row">
+                <div class="col-sm-4">@lang('device.attributes.sysContact')</div>
+                <div class="col-sm-8">{{ $contact }}</div>
+            </div>
+        @endif
+        @if($date_added)
+            <div class="row">
+                <div class="col-sm-4">@lang('device.attributes.inserted')</div>
+                <div class="col-sm-8">{{ $date_added }}</div>
+            </div>
+        @endif
     </div>
 </div>
 </div>
