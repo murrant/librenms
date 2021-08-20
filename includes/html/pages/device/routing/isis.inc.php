5185c6ebd0807df9eda5e0e637aa3e8d6d1ee3e0
ottorei <otto@ottorei.fi>
2021-08-21 01:55:19 +0300

    Fix links where port_id is missing

--- includes/html/pages/device/routing/isis.inc.php
+++ includes/html/pages/device/routing/isis.inc.php
@@ -10,14 +10,15 @@ echo '
         <thead>
           <tr>
             <th>&nbsp;</th>
-            <th>Local device</th>
-            <th>Local interface</th>
+            <th>Local Device</th>
+            <th>Local Interface</th>
             <th>Adjacent</th>
             <th>System ID</th>
             <th>Area</th>
-            <th>System type</th>
+            <th>System Type</th>
+            <th>Admin</th>
             <th>State</th>
-            <th>Last uptime</th>
+            <th>Last Uptime</th>
           </tr>
         </thead>';
 
@@ -28,22 +29,23 @@ foreach (IsisAdjacency::where('device_id', $device['device_id'])->with('port')->
         $color = 'red';
     }
     $interface_name = $adj->port->ifName;
+    $port_link = 'unknown';
+
+    if ($adj->port) {
+      $port_link = \LibreNMS\Util\Url::portLink($adj->port);
+    }
 
     echo '
         <tbody>
         <tr>
             <td></td>
             <td>' . generate_device_link($device, 0, ['tab' => 'routing', 'proto' => 'isis']) . '</td>
-            <td><a href="' . \LibreNMS\Util\Url::generate([
-        'page'=>'device',
-        'device'=>$adj->device_id,
-        'tab'=>'port',
-        'port'=>$adj->port_id,
-    ]) . '">' . $interface_name . '</a></td>
+             <td>' . $port_link . '</td>
             <td>' . $adj->isisISAdjIPAddrAddress . '</td>
             <td>' . $adj->isisISAdjNeighSysID . '</td>
             <td>' . $adj->isisISAdjAreaAddress . '</td>
             <td>' . $adj->isisISAdjNeighSysType . '</td>
+            <td>' . $adj->isisCircAdminState . '</td>
             <td><strong><span style="color: ' . $color . ';">' . $adj->isisISAdjState . '</span></strong></td>
             <td>' . \LibreNMS\Util\Time::formatInterval($adj->isisISAdjLastUpTime) . '</td>
         </tr>
--- includes/html/pages/routing/isis.inc.php
+++ includes/html/pages/routing/isis.inc.php
@@ -63,14 +63,15 @@ if (! Auth::user()->hasGlobalRead()) {
           <thead>
             <tr>
               <th>&nbsp;</th>
-              <th>Local device</th>
-              <th>Local interface</th>
+              <th>Local Device</th>
+              <th>Local Interface</th>
               <th>Adjacent</th>
               <th>System ID</th>
               <th>Area</th>
-              <th>System type</th>
+              <th>System Type</th>
+              <th>Admin</th>
               <th>State</th>
-              <th>Last uptime</th>
+              <th>Last Uptime</th>
             </tr>
           </thead>';
 
@@ -83,22 +84,23 @@ if (! Auth::user()->hasGlobalRead()) {
         }
 
         $interface_name = $adj->port->ifName;
+        $port_link = 'unknown';
+
+        if ($adj->port) {
+          $port_link = \LibreNMS\Util\Url::portLink($adj->port);
+        }
 
         echo '
           <tbody>
           <tr>
               <td></td>
               <td>' . generate_device_link($device, 0, ['tab' => 'routing', 'proto' => 'isis']) . '</td>
-              <td><a href="' . \LibreNMS\Util\Url::generate([
-            'page'=>'device',
-            'device'=>$adj->device_id,
-            'tab'=>'port',
-            'port'=>$adj->port_id,
-        ]) . '">' . $interface_name . '</a></td>
+              <td>' . $port_link . '</td>
               <td>' . $adj->isisISAdjIPAddrAddress . '</td>
               <td>' . $adj->isisISAdjNeighSysID . '</td>
               <td>' . $adj->isisISAdjAreaAddress . '</td>
               <td>' . $adj->isisISAdjNeighSysType . '</td>
+              <td>' . $adj->isisCircAdminState . '</td>
               <td><strong><span style="color: ' . $color . ';">' . $adj->isisISAdjState . '</span></strong></td>
               <td>' . \LibreNMS\Util\Time::formatInterval($adj->isisISAdjLastUpTime) . '</td>
           </tr>
