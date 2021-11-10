6e58a08d2922c5d9a4ec41dbbde2d6375b712161
Tony Murray <murraytony@gmail.com>
2021-11-10 15:47:43 -0600

    Database structure

---
+++ app/Scopes/CredentialTypeScope.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * CredentialTypeScope.php
+ *
+ * Limit query to specific kind of credential
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @link       https://www.librenms.org
+ *
+ * @copyright  2021 Tony Murray
+ * @author     Tony Murray <murraytony@gmail.com>
+ */
+
+namespace App\Scopes;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+
+class CredentialTypeScope implements Scope
+{
+    /**
+     * @var string
+     */
+    private $type;
+
+    public function __construct(string $type)
+    {
+        $this->type = $type;
+    }
+
+    public function apply(Builder $builder, Model $model)
+    {
+        $builder->where('credential_type', $this->type);
+    }
+}
