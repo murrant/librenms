c340dc3e55ce90de4e98aa4673891502ace2c301
Tony Murray <murraytony@gmail.com>
2024-06-12 07:42:10 -0500

    Add translatable strings

--- lang/en/port.php
+++ lang/en/port.php
@@ -5,4 +5,31 @@ return [
         'updated' => ':port: groups updated',
         'none' => ':port no update requested',
     ],
+    'filters' => [
+        'status_up' => 'Only Show Up',
+        'admin_down' => 'Show Admin Down',
+        'disabled' => 'Show Disabled',
+        'ignored' => 'Show Ignored',
+    ],
+    'graphs' => [
+        'bits' => 'Bits',
+        'upkts' => 'Unicast Packets',
+        'nupkts' => 'Non-Unicast Packets',
+        'errors' => 'Errors',
+    ],
+    'mtu_label' => 'MTU :mtu',
+    'tabs' => [
+        'arp' => 'ARP Table',
+        'fdb' => 'FDB Table',
+        'links' => 'Neighbors',
+        'xdsl' => 'xDSL',
+    ],
+    'vlan_count' => 'VLANs: :count',
+    'vlan_label' => 'VLAN: :label',
+    'xdsl' => [
+        'sync' => 'Sync: :down/:up',
+        'attainable' => 'Max: :down/:up',
+        'attenuation' => 'Atten: :down/:up',
+        'snr' => 'SNR: :down/:up',
+    ],
 ];
