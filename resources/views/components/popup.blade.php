4eac67b298749cb5d6ce6f2ddcade9cc0d1c8394
Tony Murray <murraytony@gmail.com>
2024-06-25 09:23:26 -0500

    Use @once on popup javascript

--- resources/views/components/popup.blade.php
+++ resources/views/components/popup.blade.php
@@ -1,32 +1,5 @@
-<div {{ $attributes->merge(['class' => 'tw-inline-block']) }} x-data="{
- popupShow: false,
- showTimeout: null,
- hideTimeout: null,
- ignoreNextShownEvent: false,
- delay: 300,
- show(timeout) {
-    clearTimeout(this.hideTimeout);
-    this.showTimeout = setTimeout(() => {
-        this.popupShow = true;
-        Popper.createPopper(this.$refs.targetRef, this.$refs.popupRef, {
-            padding: 8
-        });
-
-        // close other popups, except this one
-        this.ignoreNextShownEvent = true;
-        this.$dispatch('librenms-popup-shown', this.$el);
-     }, timeout);
- },
- hide(timeout) {
-    if (this.ignoreNextShownEvent) {
-        this.ignoreNextShownEvent = false;
-        return;
-    }
-
-     clearTimeout(this.showTimeout);
-     this.hideTimeout = setTimeout(() => this.popupShow = false, timeout)
- }
-}"
+<div {{ $attributes->merge(['class' => 'tw-inline-block']) }}
+ x-data="popup"
  x-on:click.away="hide(0)"
  x-on:librenms-popup-shown.window="() => hide(0)"
 >
@@ -53,3 +26,38 @@
         @endisset
     </div>
 </div>
+@once
+<script>
+    document.addEventListener("alpine:init", () => {
+        Alpine.data("popup", () => ({
+            popupShow: false,
+            showTimeout: null,
+            hideTimeout: null,
+            ignoreNextShownEvent: false,
+            delay: 300,
+            show(timeout) {
+                clearTimeout(this.hideTimeout);
+                this.showTimeout = setTimeout(() => {
+                    this.popupShow = true;
+                    Popper.createPopper(this.$refs.targetRef, this.$refs.popupRef, {
+                        padding: 8
+                    });
+
+                    // close other popups, except this one
+                    this.ignoreNextShownEvent = true;
+                    this.$dispatch('librenms-popup-shown', this.$el);
+                }, timeout);
+            },
+            hide(timeout) {
+                if (this.ignoreNextShownEvent) {
+                    this.ignoreNextShownEvent = false;
+                    return;
+                }
+
+                clearTimeout(this.showTimeout);
+                this.hideTimeout = setTimeout(() => this.popupShow = false, timeout)
+            }
+        }));
+    });
+</script>
+@endonce
