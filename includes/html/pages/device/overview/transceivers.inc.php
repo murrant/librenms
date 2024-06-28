53ed9d3b5cc5d4fdd33da625545937157c04ef61
Tony Murray <murraytony@gmail.com>
2024-06-27 23:51:53 -0500

    Change default sort to group by type, then channel move some code out of overview blade template Fix bad type ocnos

--- includes/html/pages/device/overview/transceivers.inc.php
+++ includes/html/pages/device/overview/transceivers.inc.php
@@ -25,5 +25,9 @@
 
 if (DeviceCache::get($device['device_id'])->transceivers->isNotEmpty()) {
     DeviceCache::get($device['device_id'])->transceivers->load(['port', 'metrics']);
-    echo view('device.overview.transceivers', ['transceivers' => DeviceCache::get($device['device_id'])->transceivers]);
+    echo view('device.overview.transceivers', [
+        'transceivers' => DeviceCache::get($device['device_id'])->transceivers,
+        'transceivers_link' => route('device', ['device' => $device['device_id'], 'tab' => 'ports', 'vars' => 'transceivers']),
+        'filterMetrics' => fn ($metrics) => $metrics->filter(fn($m) => in_array($m->type, ['power-rx', 'temperature']))->sort(fn($a, $b) => $a->defaultOrder() <=> $b->defaultOrder()),
+    ]);
 }
