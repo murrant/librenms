c2409fb1d0e7425835c0ab35407ce2c1860edeed
Tony Murray <murraytony@gmail.com>
2024-11-29 18:06:52 -0600

    Add apex charts

--- LibreNMS/Data/Source/Rrd.php
+++ LibreNMS/Data/Source/Rrd.php
@@ -3,95 +3,93 @@
 namespace LibreNMS\Data\Source;
 
 use App\Facades\LibrenmsConfig;
-use LibreNMS\Util\Number;
+use LibreNMS\Data\ChartData;
+use LibreNMS\Enum\RrdCF;
 use Symfony\Component\Process\Process;
 
 class Rrd
 {
-    private array $options;
-    private int $start;
-    private ?int $end = null;
-    private array $xport = [];
+    private array $options = [];
 
     private function __construct(
+        public int $start,
+        public ?int $end = null,
     ) {
-        $this->start = time() - 3600;
-        $rrdcached = LibrenmsConfig::get('rrdcached');
-        $this->options = [LibrenmsConfig::get('rrdtool'), 'xport'];
-        if ($rrdcached) {
-            array_push($this->options, '--daemon', $rrdcached);
-        }
     }
 
-    public static function make(): static
+    public static function make(int $start, ?int $end = null): static
     {
-        return new static;
+        return new static($start, $end);
     }
 
     public function get(): array
     {
-        $timeframe = ['--start', $this->start];
-        if ($this->end) {
-            array_push($timeframe, '--end', $this->end);
-        }
-        foreach($this->xport as $def) {
-            $this->options[] = "XPORT:$def";
-        }
 
-        $proc = new Process($this->options + $timeframe);
-        $proc->run();
-
-        $xport_data = [];
-        $timestamps = [];
-        $output = $proc->getOutput();
-        $error = $proc->getErrorOutput();
-        if($error) {
-            throw new \Exception($error);
-        }
-
-        $data = new \SimpleXMLElement($output);
-        $current_timestamp = (int) $data->meta->start;
-        $step = (int) $data->meta->step;
-
-        foreach($data->data->row as $row) {
-            foreach ($this->xport as $index => $def) {
-                $value = (string) $row->v[$index];
-                $xport_data[$def][] = $value == 'NaN' ? null : Number::cast($value);
-            }
-            $timestamps[] = $current_timestamp;
-            $current_timestamp += $step;
-        }
-
-        return [
-            'data' => $xport_data,
-            'timestamps' => $timestamps,
-        ];
     }
 
-    public function def(string $label, string $dataset, string $rrd = null, string $aggregation = 'AVERAGE'): static
+    public function def(string $label, string $dataset, string $rrd = null, RrdCF $consolidationFunction = RrdCF::AVERAGE): static
     {
         $def = "DEF:$label";
         if ($rrd) {
             $def .= "=$rrd";
         }
-        $def .= ":$dataset:$aggregation";
+        $def .= ":$dataset:$consolidationFunction->name";
 
         $this->options[] = $def;
 
         return $this;
     }
 
-    public function cdef(string $label, string $source): static
+    public function cdef(string $label, string $rpnExpression): static
     {
-        $this->options[] = "CDEF:$label=$source";
+        $this->options[] = "CDEF:$label=$rpnExpression";
 
         return $this;
     }
 
-    public function xport(array $defs): static
+    public function graph(): string
     {
-        $this->xport = $defs;
+        return $this->run('graph');
+    }
 
-        return $this;
+    public function xport(array $defs): ChartData
+    {
+        // set up XPORT options either ds only or ds => label
+        foreach($defs as $def) {
+            $this->options[] = "XPORT:$def:$def";
+        }
+
+        $xport = json_decode($this->run('xport'));
+
+        return ChartData::createFromRrd($xport->meta->legend, $xport->data);
+    }
+
+    private function run(string $command): string
+    {
+        $cli = [
+            LibrenmsConfig::get('rrdtool'),
+            $command,
+            '--showtime',
+            '--json',
+            '--start',
+            $this->start,
+        ];
+        if ($this->end) {
+            array_push($cli, '--end', $this->end);
+        }
+        $rrdcached = LibrenmsConfig::get('rrdcached');
+        if ($rrdcached) {
+            array_push($cli, '--daemon', $rrdcached);
+        }
+
+        $proc = new Process(array_merge($cli, $this->options));
+        $proc->run();
+
+        $error = $proc->getErrorOutput();
+        if($error) {
+            throw new \Exception($error);
+        }
+
+        return $proc->getOutput();
     }
 }
