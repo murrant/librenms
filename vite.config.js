459e0450cb170d6f841232599456ac563531831d
Tony Murray <murraytony@gmail.com>
2025-03-11 11:36:31 -0500

    Change output directory to html

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue2';
 
 export default defineConfig({
     plugins: [
         laravel({
+            publicDirectory: 'html',
             input: [
                 'resources/sass/app.scss',
                 'resources/js/app.js',
