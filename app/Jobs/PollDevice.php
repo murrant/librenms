c3e33fbbf9251df9608ee3fc1dee3ffdd836080f
Tony Murray <murraytony@gmail.com>
2025-06-10 13:11:35 -0500

    Refactor a bit

--- app/Jobs/PollDevice.php
+++ app/Jobs/PollDevice.php
@@ -41,7 +41,7 @@ class PollDevice implements ShouldQueue
 
     /**
      * @param  int  $device_id
-     * @param  array<string, bool|string[]>  $module_overrides
+     * @param  ModuleList  $moduleList
      */
     public function __construct(
         public int $device_id,
