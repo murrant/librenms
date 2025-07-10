717443767b89cf5b626e0b64ab2e44855bfc4710
Tony Murray <murraytony@gmail.com>
2025-07-10 01:12:43 -0500

    Validate test settings set

---
+++ tests/AATestEnvTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace LibreNMS\Tests;
+
+use App\Facades\LibrenmsConfig;
+use PHPUnit\Framework\TestCase;
+
+class AATestEnvTest extends TestCase
+{
+
+    public function testSnmpTimeoutsAreConfigured(): void
+    {
+        $this->assertEquals(0.1, LibrenmsConfig::get('snmp.timeout'));
+        $this->assertEquals(0, LibrenmsConfig::get('snmp.retries'));
+    }
+}
