abb8a69e19459a765428f2e4757bbed12ce5a84b
Tony Murray <murraytony@gmail.com>
2026-04-29 02:15:08 -0500

    Initial working

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,7 +8,7 @@ import "../css/app.css";
 import "./bootstrap";
 
 import Vue from "vue";
-import {i18n} from "./plugins/i18n.js"; // translation
+import { i18n } from "./plugins/i18n.js"; // translation
 import ToggleButton from "vue-js-toggle-button";
 import VTooltip from "v-tooltip";
 import vSelect from "vue-select";
@@ -20,7 +20,8 @@ import Alpine from "alpinejs";
 // import popup from './components/alpine/popup.js'
 import popup from "./components/alpine/oldpopup.js";
 import deviceLink from "./components/alpine/deviceLink.js";
-import LibreNMSDate from './datetime.js';
+import filterBarComponent from "./components/alpine/filterBarComponent.js";
+import LibreNMSDate from "./datetime.js";
 
 window.LibreNMS = window.LibreNMS || {};
 window.LibreNMS.Date = LibreNMSDate;
@@ -75,6 +76,7 @@ const app = new Vue({
 
 Alpine.data('popup', popup);
 Alpine.data('deviceLink', deviceLink);
+Alpine.data("filterBarComponent", filterBarComponent);
 
 window.Alpine = Alpine;
 Alpine.start();
