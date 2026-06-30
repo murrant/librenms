0fd32acaa836e6365dfcefc96c85dfeaf34c13ec
Tony Murray <murraytony@gmail.com>
2026-06-30 16:59:21 -0500

    New Graph Interface

---
+++ app/Console/Commands/GraphDetail.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Facades\DeviceCache;
+use App\Models\Device;
+use Illuminate\Console\Command;
+use LibreNMS\Data\Graphing\GraphFactory;
+
+class GraphDetail extends Command
+{
+    protected $signature = 'graph:detail {name} {--device=}';
+    protected $description = 'Show info about a given graph';
+
+    public function handle(GraphFactory $graphs): int
+    {
+        $device = DeviceCache::get($this->option('device') ?: Device::limit(1)->value('device_id'));
+        DeviceCache::setPrimary($device->device_id);
+
+        $graph = $graphs->graphFor($this->argument('name'));
+        $def = $graph->definition([
+            'type' => $this->argument('name'),
+        ]);
+
+
+        $this->line("Type: $graph->type");
+        $this->line("Subtype: $graph->subtype");
+        $this->line('Authorized: ' . ($graph->authorize() ? 'true' : 'false'));
+        $this->line("Graph Title: " . $graph->getGraphTitle());
+        $this->line("Page Title: " . substr($graph->getPageTitle(), 0, 80));
+        $this->line("Rrd Files: " . implode(', ', array_map(fn($f) => basename($f), $graph->getRrdFiles())));
+        $this->line("Definition: " . substr(implode(' ', $def), 80));
+
+
+        return 0;
+    }
+}
