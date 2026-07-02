029842ffa0575214786a90a48ac625a536c57c6b
Tony Murray <murraytony@gmail.com>
2026-07-02 10:08:34 -0500

    fix some issues

--- app/Console/Commands/GraphDetail.php
+++ app/Console/Commands/GraphDetail.php
@@ -9,7 +9,7 @@ use LibreNMS\Data\Graphing\GraphFactory;
 
 class GraphDetail extends Command
 {
-    protected $signature = 'graph:detail {name} {--device=}';
+    protected $signature = 'graph:detail {name} {--device=} {--full}';
     protected $description = 'Show info about a given graph';
 
     public function handle(GraphFactory $graphs): int
@@ -21,13 +21,15 @@ class GraphDetail extends Command
             'device' => $device->device_id,
         ]);
 
-        $this->line("Type: $graph->type");
-        $this->line("Subtype: $graph->subtype");
+        $trim = $this->option('full') ? null : 80;
+
+        $this->line("Type: {$graph->getParams()->title}");
+        $this->line("Subtype: {$graph->getParams()->subtype}");
         $this->line('Authorized: ' . ($graph->authorize() ? 'true' : 'false'));
         $this->line('Graph Title: ' . $graph->getGraphTitle());
-        $this->line('Page Title: ' . substr($graph->getPageTitle(), 0, 80));
+        $this->line('Page Title: ' . substr($graph->getPageTitle(), 0, $trim));
         $this->line('Rrd Files: ' . implode(', ', array_map(fn ($f) => basename($f), $graph->getRrdFiles())));
-        $this->line('Definition: ' . substr(implode(' ', $graph->rrdDefinition()), 80));
+        $this->line('Definition: ' . substr(implode(' ', $graph->rrdDefinition()), $trim));
 
         return 0;
     }
